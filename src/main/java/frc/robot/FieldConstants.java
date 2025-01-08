a46463c5b6dd348df2076a497bcdd749969b96eb
Kev1n8088 <jebidiahkerman828@gmail.com>
2025-01-07 20:12:01 -0500

    Added some basic constants, getReefPositionPose function, and getRedPosition function for switching blue alliance pos to red alliance (and vice versa)

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -1,5 +1,56 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
+
 public class FieldConstants {
+    //All units are in meters and radians. 
+    //All measurements with Blue Team Right Corner as origin, towards Red Team +X, towards Blue Team Left Corner +Y. 
+    //All locations are for Blue Team. Convert to Red Team by getting distance to field center and inverting. 
+
+    public static final double FIELD_LENGTH = Units.inchesToMeters(690.88);
+    public static final double FIELD_WIDTH = Units.inchesToMeters(317.15);
+
+    public static final Pose3d PROCESSOR_OPENING = new Pose3d(Units.inchesToMeters(0), Units.inchesToMeters(0),
+            Units.inchesToMeters(0), new Rotation3d());
+
+    public static final Pose3d LEFT_CORAL_STATION = new Pose3d(Units.inchesToMeters(0), Units.inchesToMeters(0),
+            Units.inchesToMeters(0), new Rotation3d());
+    
+    public static final Pose3d RIGHT_CORAL_STATION = new Pose3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0),
+            new Rotation3d());
+
+    public static final Pose2d REEF_CENTER = new Pose2d(Units.inchesToMeters(144.0 + 65.49 / 2.0), FIELD_WIDTH / 2.0,new Rotation2d());
+    
+    public static final double REEF_L1_HEIGHT = 0.46;
+    public static final double REEF_L2_HEIGHT = 0.81;
+    public static final double REEF_L3_HEIGHT = 1.21;
+    public static final double REEF_L4_HEIGHT = 1.83;
+
+    public static final double REEF_LEVEL_HEIGHTS[] = {REEF_L1_HEIGHT, REEF_L2_HEIGHT, REEF_L3_HEIGHT, REEF_L4_HEIGHT}; 
+
+    public static final int REEF_SIDES = 6;
+    public static final int REEF_PLACES_PER_SIDE = 2;
+    public static final double REEF_RADIUS = Units.inchesToMeters(65.49) / 2.0; //Inscribed circle radius of the reef
+    public static final double REEF_RADIUS_OFFSET = Units.inchesToMeters(12.94) / 2.0; //Distance each reef place is from the radius above
+    
+    public static final double REEF_L1_INSET = 0.1;
+    public static final double REEF_L2_INSET = 0.041;
+    public static final double REEF_L3_INSET = 0.041;
+    public static final double REEF_L4_INSET = 0.027;
+
+    public static final double REEF_LEVEL_RADII[] = {REEF_RADIUS - REEF_L1_INSET, REEF_RADIUS - REEF_L2_INSET, REEF_RADIUS - REEF_L3_INSET, REEF_RADIUS - REEF_L4_INSET};
+
+    public static final double REEF_L1_ANGLE = Units.degreesToRadians(10.0);
+    public static final double REEF_L2_ANGLE = Units.degreesToRadians(-35.0);
+    public static final double REEF_L3_ANGLE = Units.degreesToRadians(-35.0);
+    public static final double REEF_L4_ANGLE = Units.degreesToRadians(-90.0);
+
+    public static final double REEF_LEVEL_ANGLES[] = {REEF_L1_ANGLE, REEF_L2_ANGLE, REEF_L3_ANGLE, REEF_L4_ANGLE};
+
+
     
 }
--- src/main/java/frc/utils/TargetAlign.java
+++ src/main/java/frc/utils/TargetAlign.java
@@ -1,5 +1,62 @@
 package frc.utils;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.FieldConstants;
+
 public class TargetAlign {
+    public Pose3d getReefPositionPose(int level, int position) {
+        double height = FieldConstants.REEF_LEVEL_HEIGHTS[level];
+        double radius = FieldConstants.REEF_LEVEL_RADII[level];
+        double verticalAngle = FieldConstants.REEF_LEVEL_ANGLES[level]; //0 is horizontal, positive is angle up
+
+        int side = position / FieldConstants.REEF_PLACES_PER_SIDE;
+        int place = position % FieldConstants.REEF_PLACES_PER_SIDE;
+
+        double horizontalAngle = 2.0 * Math.PI / FieldConstants.REEF_SIDES * side; //0 is facing the -X direction, moving counterclockwise. Will normalize with convention (+X is 0) on return
+        double offsetDirection = (place == 0) ? -1 : 1;
+
+        double x = -radius * Math.cos(horizontalAngle) + offsetDirection * FieldConstants.REEF_RADIUS_OFFSET * Math.sin(horizontalAngle);
+        double y = -radius * Math.sin(horizontalAngle) - offsetDirection * FieldConstants.REEF_RADIUS_OFFSET * Math.cos(horizontalAngle);
+
+
+        var alliance = DriverStation.getAlliance();
+        boolean invert = false;
+
+        if(alliance.isPresent() && alliance.get() == Alliance.Red) {
+            invert = true;
+        }
+
+        if(invert) {
+            Pose2d pos = getRedPosition(new Pose2d(x, y, new Rotation2d(horizontalAngle)));
+            x = pos.getX();
+            y = pos.getY();
+            horizontalAngle = pos.getRotation().getRadians();
+        }
+
+        Pose3d pose = new Pose3d(x, y, height, new Rotation3d(0,verticalAngle, horizontalAngle + (Math.PI)));
+        return pose;
+    }
+
+    //Converts from blue alliance pos to red alliance pos. Takes the difference of the center of the field and the blue position, and adds it back to the center position. Also inverts angle. 
+    public Pose2d getRedPosition(Pose2d bluePosition) {
+        double x = bluePosition.getX();
+        double y = bluePosition.getY();
+        double angle = bluePosition.getRotation().getRadians();
+
+        double distanceFromCenterX = FieldConstants.FIELD_LENGTH / 2.0 - x;
+        double distanceFromCenterY = FieldConstants.FIELD_WIDTH / 2.0 - y;
+
+        x = FieldConstants.FIELD_LENGTH / 2.0 + distanceFromCenterX;
+        y = FieldConstants.FIELD_WIDTH / 2.0 + distanceFromCenterY;
+
+        angle = -angle;
+
+        return new Pose2d(x, y, new Rotation2d(angle));
+    }
     
 }
