812a5bd1b733f4b769c780a096dd185f44d05eab
QuinnKenison <quinnkenison@gmail.com>
2025-01-21 17:36:59 -0500

    Set basic constants
    - Create constant for CAN ID
    - Create constants for PID values (kP, kI, kD)
    - Create constant for amp limit

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -10,6 +10,17 @@ public class Elevator extends SubsystemBase
         implements BaseLinearMechanism<Elevator.Constants.Position> {
     /** Constant values of elevator subsystem. */
     public static final class Constants {
+        private static final class CANIDs {
+            private static final int elevatorMotor = 0; //TODO get actual can ids 
+        }
+        public static class PID {
+            public static final double kP = 0; //TODO find good value
+            public static final double kI = 0; //TODO find good value
+            public static final double kD = 0; //TODO find good value
+        }
+
+        public static final int MAX_AMPS = 10;
+
         /** Positions that elevator subsystem can be in. */
         public enum Position {
         }
