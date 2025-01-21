85e69c04c48565999571321ab7525f2e7b2a469a
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-01-21 00:28:01 -0500

    add manipulator subsystem with interface methods

---
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import com.techhounds.houndutil.houndlib.subsystems.BaseIntake;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Subsystem which intakes and scores scoring elements. */
+public class Manipulator extends SubsystemBase implements BaseIntake {
+    /** Constant values of manipulator subsystem. */
+    public static final class Constants {
+    }
+
+    @Override
+    public Command runRollersCommand() {
+        // TODO Auto-generated method stubs
+        throw new UnsupportedOperationException("Unimplemented method 'runRollersCommand'");
+    }
+
+    @Override
+    public Command reverseRollersCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reverseRollersCommand'");
+    }
+}
