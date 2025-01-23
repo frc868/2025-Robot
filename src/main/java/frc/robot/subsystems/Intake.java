966d46b06317d5b9a3d169e411ee7c8b3dca168f
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-01-22 22:28:35 -0500

    add intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,98 @@
+package frc.robot.subsystems;
+
+import java.util.function.Supplier;
+
+import com.techhounds.houndutil.houndlib.subsystems.BaseIntake;
+import com.techhounds.houndutil.houndlib.subsystems.BaseSingleJointedArm;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Intake.Constants.Position;
+
+/** Intake subsystem which intakes algae from ground. */
+public class Intake extends SubsystemBase implements BaseIntake, BaseSingleJointedArm<Intake.Constants.Position> {
+    /** Constant values of intake subsystem. */
+    public static final class Constants {
+        /** Positions that intake subsystem can be in. */
+        public enum Position {
+        }
+    }
+
+    @Override
+    public double getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public void resetPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resetPosition'");
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVoltage'");
+    }
+
+    @Override
+    public Command moveToCurrentGoalCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveToCurrentGoalCommand'");
+    }
+
+    @Override
+    public Command moveToPositionCommand(Supplier<Position> goalPositionSupplier) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveToPositionCommand'");
+    }
+
+    @Override
+    public Command moveToArbitraryPositionCommand(Supplier<Double> goalPositionSupplier) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveToArbitraryPositionCommand'");
+    }
+
+    @Override
+    public Command movePositionDeltaCommand(Supplier<Double> delta) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'movePositionDeltaCommand'");
+    }
+
+    @Override
+    public Command holdCurrentPositionCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'holdCurrentPositionCommand'");
+    }
+
+    @Override
+    public Command resetPositionCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resetPositionCommand'");
+    }
+
+    @Override
+    public Command setOverridenSpeedCommand(Supplier<Double> speed) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setOverridenSpeedCommand'");
+    }
+
+    @Override
+    public Command coastMotorsCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'coastMotorsCommand'");
+    }
+
+    @Override
+    public Command runRollersCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runRollersCommand'");
+    }
+
+    @Override
+    public Command reverseRollersCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reverseRollersCommand'");
+    }
+}
