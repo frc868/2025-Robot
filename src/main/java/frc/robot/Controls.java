9c594de1483852ecb16f92bfc71943c4569dd632
ZorzalG <nickwillimJT2@gmail.com>
2025-01-29 17:47:09 -0500

    ctrl+c ctrl+v from 2024

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,8 +1,37 @@
 package frc.robot;
 
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Pivot;
+
 /**
  * Controls configurations for each member of drive team.
  * Each member has a static method which configures their controls.
  */
 public class Controls {
+
+    public static void configureDriverControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+
+    }
+
+    public static void configureOperatorControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+
+    }
+
+    public static void configureOverridesControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+
+    }
+
+    public static void configureTestingControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+
+    }
+
 }
