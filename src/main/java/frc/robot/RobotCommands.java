ea34faa566cca91dc6347eba120b41265be961bd
ZorzalG <nickwillimJT2@gmail.com>
2025-01-29 17:49:56 -0500

    basic robot command

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -1,5 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+
 /** Robot commands which involve multiple subsystems. */
 public class RobotCommands {
+
+    public static Command climbCommand(Climber climber) {
+        throw new UnsupportedOperationException("Unimplemented command 'climbCommand'");
+    }
+
 }
