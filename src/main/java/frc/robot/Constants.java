171cdc747e034e21545014cb8c30f063b8fce3e9
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-02-02 20:30:24 -0500

    update controls

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,117 +1,71 @@
 package frc.robot;
 
-import com.pathplanner.lib.config.ModuleConfig;
-import com.pathplanner.lib.config.RobotConfig;
-import com.techhounds.houndutil.houndlib.AprilTagPhotonCamera.PhotonCameraConstants;
-import com.techhounds.houndutil.houndlib.leds.BaseLEDSection;
-
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
-
-import com.techhounds.houndutil.houndlib.swerve.CoaxialSwerveModule.SwerveConstants;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.system.plant.DCMotor;
 
 public class Constants {
-        enum ControllerType {
-                XboxController,
-                FlightStick
-        }
+    public static final class Field {
+        public static final double FIELD_LENGTH = Units.inchesToMeters(690.88);
+        public static final double FIELD_WIDTH = Units.inchesToMeters(317.15);
 
-        public static final boolean DEBUG_MODE = false;
+        public static final Pose3d PROCESSOR_OPENING = new Pose3d(Units.inchesToMeters(0),
+                Units.inchesToMeters(0),
+                Units.inchesToMeters(0), new Rotation3d());
 
-        public static final ControllerType CONTROLLER_TYPE = ControllerType.FlightStick;
+        public static final Pose3d LEFT_CORAL_STATION = new Pose3d(Units.inchesToMeters(0),
+                Units.inchesToMeters(0),
+                Units.inchesToMeters(0), new Rotation3d());
 
-        public static final double PERIOD = 0.020;
+        public static final Pose3d RIGHT_CORAL_STATION = new Pose3d(Units.inchesToMeters(0),
+                Units.inchesToMeters(0), Units.inchesToMeters(0),
+                new Rotation3d());
 
-        public static final class Teleop {
-                /**
-                 * A value inputted into the rate limiter (the joystick input) can move from 0
-                 * to 1 in 1/RATE_LIMIT seconds.
-                 * 
-                 * A rate limit of 3, for example, means that 0->1 in 1/3 sec.
-                 * Larger numbers mean less of a rate limit.
-                 */
-                public static final double JOYSTICK_INPUT_RATE_LIMIT = 15.0; // TODO
-                public static final double JOYSTICK_INPUT_DEADBAND = 0.05; // TODO
-                public static final double JOYSTICK_CURVE_EXP = 2; // TODO
-                public static final double JOYSTICK_ROT_CURVE_EXP = 1; // TODO
-        }
+        public static final Pose2d REEF_CENTER = new Pose2d(Units.inchesToMeters(144.0 + 65.49 / 2.0),
+                FIELD_WIDTH / 2.0, new Rotation2d());
 
-        public static final class Field {
-                public static final double FIELD_LENGTH = Units.inchesToMeters(690.88);
-                public static final double FIELD_WIDTH = Units.inchesToMeters(317.15);
+        public static final double REEF_L1_HEIGHT = 0.46;
+        public static final double REEF_L2_HEIGHT = 0.81;
+        public static final double REEF_L3_HEIGHT = 1.21;
+        public static final double REEF_L4_HEIGHT = 1.83;
 
-                public static final Pose3d PROCESSOR_OPENING = new Pose3d(Units.inchesToMeters(0),
-                                Units.inchesToMeters(0),
-                                Units.inchesToMeters(0), new Rotation3d());
+        public static final double REEF_LEVEL_HEIGHTS[] = { REEF_L1_HEIGHT, REEF_L2_HEIGHT, REEF_L3_HEIGHT,
+                REEF_L4_HEIGHT };
 
-                public static final Pose3d LEFT_CORAL_STATION = new Pose3d(Units.inchesToMeters(0),
-                                Units.inchesToMeters(0),
-                                Units.inchesToMeters(0), new Rotation3d());
+        public static final int REEF_SIDES = 6;
+        public static final int REEF_PLACES_PER_SIDE = 2;
+        public static final double REEF_RADIUS = Units.inchesToMeters(65.49) / 2.0; // Inscribed circle radius
+                                                                                    // of the reef
+        public static final double REEF_RADIUS_OFFSET = Units.inchesToMeters(12.94) / 2.0; // Distance each reef
+                                                                                           // place is from the
+                                                                                           // radius above
 
-                public static final Pose3d RIGHT_CORAL_STATION = new Pose3d(Units.inchesToMeters(0),
-                                Units.inchesToMeters(0), Units.inchesToMeters(0),
-                                new Rotation3d());
+        public static final double REEF_L1_INSET = 0.1;
+        public static final double REEF_L2_INSET = 0.041;
+        public static final double REEF_L3_INSET = 0.041;
+        public static final double REEF_L4_INSET = 0.027;
 
-                public static final Pose2d REEF_CENTER = new Pose2d(Units.inchesToMeters(144.0 + 65.49 / 2.0),
-                                FIELD_WIDTH / 2.0, new Rotation2d());
+        public static final double REEF_LEVEL_RADII[] = { REEF_RADIUS - REEF_L1_INSET,
+                REEF_RADIUS - REEF_L2_INSET, REEF_RADIUS - REEF_L3_INSET, REEF_RADIUS - REEF_L4_INSET };
 
-                public static final double REEF_L1_HEIGHT = 0.46;
-                public static final double REEF_L2_HEIGHT = 0.81;
-                public static final double REEF_L3_HEIGHT = 1.21;
-                public static final double REEF_L4_HEIGHT = 1.83;
+        public static final double REEF_L1_ANGLE = Units.degreesToRadians(10.0);
+        public static final double REEF_L2_ANGLE = Units.degreesToRadians(-35.0);
+        public static final double REEF_L3_ANGLE = Units.degreesToRadians(-35.0);
+        public static final double REEF_L4_ANGLE = Units.degreesToRadians(-90.0);
 
-                public static final double REEF_LEVEL_HEIGHTS[] = { REEF_L1_HEIGHT, REEF_L2_HEIGHT, REEF_L3_HEIGHT,
-                                REEF_L4_HEIGHT };
+        public static final double REEF_LEVEL_ANGLES[] = { REEF_L1_ANGLE, REEF_L2_ANGLE, REEF_L3_ANGLE,
+                REEF_L4_ANGLE };
+    }
 
-                public static final int REEF_SIDES = 6;
-                public static final int REEF_PLACES_PER_SIDE = 2;
-                public static final double REEF_RADIUS = Units.inchesToMeters(65.49) / 2.0; // Inscribed circle radius
-                                                                                            // of the reef
-                public static final double REEF_RADIUS_OFFSET = Units.inchesToMeters(12.94) / 2.0; // Distance each reef
-                                                                                                   // place is from the
-                                                                                                   // radius above
-
-                public static final double REEF_L1_INSET = 0.1;
-                public static final double REEF_L2_INSET = 0.041;
-                public static final double REEF_L3_INSET = 0.041;
-                public static final double REEF_L4_INSET = 0.027;
-
-                public static final double REEF_LEVEL_RADII[] = { REEF_RADIUS - REEF_L1_INSET,
-                                REEF_RADIUS - REEF_L2_INSET, REEF_RADIUS - REEF_L3_INSET, REEF_RADIUS - REEF_L4_INSET };
-
-                public static final double REEF_L1_ANGLE = Units.degreesToRadians(10.0);
-                public static final double REEF_L2_ANGLE = Units.degreesToRadians(-35.0);
-                public static final double REEF_L3_ANGLE = Units.degreesToRadians(-35.0);
-                public static final double REEF_L4_ANGLE = Units.degreesToRadians(-90.0);
-
-                public static final double REEF_LEVEL_ANGLES[] = { REEF_L1_ANGLE, REEF_L2_ANGLE, REEF_L3_ANGLE,
-                                REEF_L4_ANGLE };
-        }
-
-        public static final class HoundBrian {
-                public static final int BUTTON_1 = 3;
-                public static final int BUTTON_2 = 4;
-                public static final int BUTTON_3 = 5;
-                public static final int BUTTON_4 = 6;
-                public static final int BUTTON_5 = 7;
-                public static final int BUTTON_6 = 8;
-                public static final int BUTTON_7 = 9;
-        }
+    public static final class HoundBrian {
+        public static final int BUTTON_1 = 3;
+        public static final int BUTTON_2 = 4;
+        public static final int BUTTON_3 = 5;
+        public static final int BUTTON_4 = 6;
+        public static final int BUTTON_5 = 7;
+        public static final int BUTTON_6 = 8;
+        public static final int BUTTON_7 = 9;
+    }
 }
--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -3,32 +3,46 @@ package frc.robot;
 import com.techhounds.houndutil.houndlib.oi.CommandVirpilJoystick;
 import com.techhounds.houndutil.houndlog.loggers.TunableDouble;
 
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.LEDs;
 
 /**
- * Controls configurations for each member of drive team.
- * Each member has a static method which configures their controls.
+ * Controls configurations for robot.
  */
 public class Controls {
     public static final class Constants {
-        public static final TunableDouble DEMO_SPEED = new TunableDouble("DEMO_SPEED", 1.0);
+        enum ControllerType {
+            XboxController,
+            FlightStick
+        }
 
+        public static final boolean DEBUG_MODE = false;
+
+        public static final ControllerType CONTROLLER_TYPE = ControllerType.FlightStick;
+
+        public static final double PERIOD = 0.020;
+
+        public static final class Teleop {
+            /**
+             * A value inputted into the rate limiter (the joystick input) can move from 0
+             * to 1 in 1/RATE_LIMIT seconds.
+             * 
+             * A rate limit of 3, for example, means that 0->1 in 1/3 sec.
+             * Larger numbers mean less of a rate limit.
+             */
+            public static final double JOYSTICK_INPUT_RATE_LIMIT = 15.0; // TODO
+            public static final double JOYSTICK_INPUT_DEADBAND = 0.05; // TODO
+            public static final double JOYSTICK_CURVE_EXP = 2; // TODO
+            public static final double JOYSTICK_ROT_CURVE_EXP = 1; // TODO
+        }
+
+        public static final TunableDouble DEMO_SPEED = new TunableDouble("DEMO_SPEED", 1.0);
     }
 
     /**
-     * Configures driver controls on a Virpil Controls Alpha-R joystick.
+     * Configure driver controls on a Virpil Controls Alpha-R joystick.
      * 
-     * @param port        the port that the joystick is connected to
-     * @param drivetrain  the drivetrain
-     * @param intake      the intake
-     * @param shooter     the shooter
-     * @param shooterTilt the shooter tilt
-     * @param climber     the climber
-     * @param leds        the LEDs
+     * @param port       port that joystick is connected to
+     * @param drivetrain drivetrain subsystem object
      */
     public static void configureDriverControls(int port, Drivetrain drivetrain) {
         CommandVirpilJoystick joystick = new CommandVirpilJoystick(port);
@@ -40,4 +54,32 @@ public class Controls {
                         () -> -joystick.getTwist() * Constants.DEMO_SPEED.get()));
     }
 
+    /**
+     * Configure operator controls on Xbox controller for manual overrides in case
+     * automatic features stop working.
+     * 
+     * @param port       port that controller is connected to
+     * @param drivetrain drivetrain subsystem object
+     */
+    public static void configureOperatorControls(int port, Drivetrain drivetrain) {
+    }
+
+    /**
+     * Configure override controls on Xbox controller for overriding mechanism
+     * safeties.
+     * 
+     * @param port       port that controller is connected to
+     * @param drivetrain drivetrain subsystem object
+     */
+    public static void configureOverrideControls(int port, Drivetrain drivetrain) {
+    }
+
+    /**
+     * Configure controls on Xbox controller for running SysId routines.
+     * 
+     * @param port       port that controller is connected to
+     * @param drivetrain drivetrain subsystem object
+     */
+    public static void configureSysIdControls(int port, Drivetrain drivetrain) {
+    }
 }
