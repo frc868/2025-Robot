e2de4ee3b8a436f259ffa86f0378ebe4263ef96f
TehYeetGod <rajrishi.sangolli@gmail.com>
2025-02-04 18:15:47 -0500

    add initial drivercontrols

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import com.techhounds.houndutil.houndlib.oi.CommandVirpilJoystick;
+
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
@@ -14,24 +17,38 @@ import frc.robot.subsystems.Pivot;
  */
 public class Controls {
 
-    public static void configureDriverControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
-            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+        public static void configureDriverControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+                        Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+                CommandVirpilJoystick joystick = new CommandVirpilJoystick(port);
+                new Trigger(() -> (Math.abs(joystick.getTwist()) > 0.05))
+                                .whileTrue(drivetrain.disableControlledRotateCommand());
 
-    }
+                joystick.stickButton().onTrue(drivetrain.resetGyroCommand());
 
-    public static void configureOperatorControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
-            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+                joystick.centerBottomHatUp()
+                                .whileTrue(elevator.moveToPositionCommand(() -> Elevator.Constants.Position.L4));
+                joystick.centerBottomHatDown()
+                                .whileTrue(elevator.moveToPositionCommand(() -> Elevator.Constants.Position.L2));
+                joystick.centerBottomHatLeft()
+                                .whileTrue(elevator.moveToPositionCommand(() -> Elevator.Constants.Position.L1));
+                joystick.centerBottomHatRight()
+                                .whileTrue(elevator.moveToPositionCommand(() -> Elevator.Constants.Position.L3));
 
-    }
+        }
 
-    public static void configureOverridesControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
-            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+        public static void configureOperatorControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+                        Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
 
-    }
+        }
 
-    public static void configureTestingControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
-            Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+        public static void configureOverridesControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+                        Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
 
-    }
+        }
+
+        public static void configureTestingControls(int port, Drivetrain drivetrain, Intake intake, Elevator elevator,
+                        Pivot pivot, Manipulator manipulator, Climber climber, LEDs leds) {
+
+        }
 
 }
