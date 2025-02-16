b28fcc877119b82d936c47472fc440b5dcb7808b
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-02-15 19:16:21 -0500

    add operator and sysid controls

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,9 +1,16 @@
 package frc.robot;
 
 import com.techhounds.houndutil.houndlib.oi.CommandVirpilJoystick;
-import com.techhounds.houndutil.houndlog.loggers.TunableDouble;
 
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Pivot;
 
 /**
  * Controls configurations for robot.
@@ -34,8 +41,6 @@ public class Controls {
             public static final double JOYSTICK_CURVE_EXP = 2; // TODO
             public static final double JOYSTICK_ROT_CURVE_EXP = 1; // TODO
         }
-
-        public static final TunableDouble DEMO_SPEED = new TunableDouble("DEMO_SPEED", 1.0);
     }
 
     /**
@@ -44,14 +49,13 @@ public class Controls {
      * @param port       port that joystick is connected to
      * @param drivetrain drivetrain subsystem object
      */
-    public static void configureDriverControls(int port, Drivetrain drivetrain) {
+    public static void configureDriverControls(int port, Drivetrain drivetrain, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Intake intake, Climber climber, LEDs leds) {
         CommandVirpilJoystick joystick = new CommandVirpilJoystick(port);
 
         drivetrain.setDefaultCommand(
-                drivetrain.teleopDriveCommand(
-                        () -> -joystick.getY() * Constants.DEMO_SPEED.get(),
-                        () -> -joystick.getX() * Constants.DEMO_SPEED.get(),
-                        () -> -joystick.getTwist() * Constants.DEMO_SPEED.get()));
+                drivetrain.teleopDriveCommand(() -> -joystick.getY(), () -> -joystick.getX(),
+                        () -> -joystick.getTwist()));
     }
 
     /**
@@ -61,7 +65,17 @@ public class Controls {
      * @param port       port that controller is connected to
      * @param drivetrain drivetrain subsystem object
      */
-    public static void configureOperatorControls(int port, Drivetrain drivetrain) {
+    public static void configureOperatorControls(int port, Drivetrain drivetrain, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Intake intake, Climber climber, LEDs leds) {
+        CommandXboxController controller = new CommandXboxController(port);
+
+        controller.y().whileTrue(elevator.setOverridenSpeedCommand(() -> -controller.getLeftY() * 0.1));
+        controller.b().whileTrue(pivot.setOverridenSpeedCommand(() -> -controller.getLeftY() * 0.25));
+        controller.a().whileTrue(intake.setOverridenSpeedCommand(() -> -controller.getLeftY() * 0.25));
+        controller.x().whileTrue(climber.setOverridenSpeedCommand(() -> -controller.getLeftY() * 0.25));
+
+        controller.rightBumper().whileTrue(manipulator.runRollersCommand());
+        controller.leftBumper().whileTrue(manipulator.reverseRollersCommand());
     }
 
     /**
@@ -71,7 +85,8 @@ public class Controls {
      * @param port       port that controller is connected to
      * @param drivetrain drivetrain subsystem object
      */
-    public static void configureOverrideControls(int port, Drivetrain drivetrain) {
+    public static void configureOverrideControls(int port, Drivetrain drivetrain, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Intake intake, Climber climber, LEDs leds) {
     }
 
     /**
@@ -80,6 +95,28 @@ public class Controls {
      * @param port       port that controller is connected to
      * @param drivetrain drivetrain subsystem object
      */
-    public static void configureSysIdControls(int port, Drivetrain drivetrain) {
+    public static void configureSysIdControls(int port, Drivetrain drivetrain, Elevator elevator,
+            Pivot pivot, Manipulator manipulator, Intake intake, Climber climber, LEDs leds) {
+        CommandXboxController controller = new CommandXboxController(port);
+
+        controller.x().and(controller.povUp()).whileTrue(drivetrain.sysIdDriveQuasistatic(Direction.kForward));
+        controller.y().and(controller.povUp()).whileTrue(drivetrain.sysIdDriveQuasistatic(Direction.kReverse));
+        controller.a().and(controller.povUp()).whileTrue(drivetrain.sysIdDriveDynamic(Direction.kForward));
+        controller.b().and(controller.povUp()).whileTrue(drivetrain.sysIdDriveDynamic(Direction.kReverse));
+
+        controller.x().and(controller.povDown()).whileTrue(elevator.sysIdQuasistatic(Direction.kForward));
+        controller.y().and(controller.povDown()).whileTrue(elevator.sysIdQuasistatic(Direction.kReverse));
+        controller.a().and(controller.povDown()).whileTrue(elevator.sysIdDynamic(Direction.kForward));
+        controller.b().and(controller.povDown()).whileTrue(elevator.sysIdDynamic(Direction.kReverse));
+
+        controller.x().and(controller.povLeft()).whileTrue(pivot.sysIdQuasistatic(Direction.kForward));
+        controller.y().and(controller.povLeft()).whileTrue(pivot.sysIdQuasistatic(Direction.kReverse));
+        controller.a().and(controller.povLeft()).whileTrue(pivot.sysIdDynamic(Direction.kForward));
+        controller.b().and(controller.povLeft()).whileTrue(pivot.sysIdDynamic(Direction.kReverse));
+
+        controller.x().and(controller.povRight()).whileTrue(intake.sysIdQuasistatic(Direction.kForward));
+        controller.y().and(controller.povRight()).whileTrue(intake.sysIdQuasistatic(Direction.kReverse));
+        controller.a().and(controller.povRight()).whileTrue(intake.sysIdDynamic(Direction.kForward));
+        controller.b().and(controller.povRight()).whileTrue(intake.sysIdDynamic(Direction.kReverse));
     }
 }
