4365f1af632a4a9a665e9f89b05f9cbd8b6931e9
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-02-15 19:26:56 -0500

    add subsystems in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,11 +14,23 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Pivot;
 
 public class RobotContainer {
     @Log(groups = "subsystems")
     private final Drivetrain drivetrain = new Drivetrain();
+    private final Intake intake = new Intake();
+    private final Elevator elevator = new Elevator();
+    private final Pivot pivot = new Pivot();
+    private final Manipulator manipulator = new Manipulator();
+    private final Climber climber = new Climber();
+    private final LEDs leds = new LEDs();
 
     @SendableLog(groups = "wpilib")
     private final CommandScheduler commandScheduler = CommandScheduler.getInstance();
@@ -45,7 +57,10 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        Controls.configureDriverControls(0, drivetrain);
+        Controls.configureDriverControls(0, drivetrain, elevator, pivot, manipulator, intake, climber, leds);
+        Controls.configureOperatorControls(1, drivetrain, elevator, pivot, manipulator, intake, climber, leds);
+        Controls.configureOverrideControls(2, drivetrain, elevator, pivot, manipulator, intake, climber, leds);
+        Controls.configureSysIdControls(3, drivetrain, elevator, pivot, manipulator, intake, climber, leds);
     }
 
     public Command getAutonomousCommand() {
