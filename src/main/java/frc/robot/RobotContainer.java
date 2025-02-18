a4e641cf699efa03473fa99ebb7b05d450ae29b7
Jazib225 <jazibrq@hotmail.com>
2025-02-18 18:01:25 -0500

    Called in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.LEDs;
 
 public class RobotContainer {
+
+    private final LEDs leds = new LEDs();
+
     public RobotContainer() {
         configureBindings();
     }
@@ -18,4 +22,9 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
     }
+
+    public LEDs getLEDs() {
+        return leds;
+    }
+
 }
