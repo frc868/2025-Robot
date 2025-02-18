ef9be8afd827c1c1b6718b2c40c3d9e846f83fea
Jazib225 <jazibrq@hotmail.com>
2025-02-18 18:28:12 -0500

    updated robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,24 @@
 
 package frc.robot;
 
+import com.techhounds.houndutil.houndlog.annotations.Log;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.LEDs;
 
 public class RobotContainer {
 
     private final LEDs leds = new LEDs();
 
+    // Add all subsystems later
+    @Log(groups = "subsystems")
+    private final HoundBrian HoundBrian = new HoundBrian(leds);
+
     public RobotContainer() {
         configureBindings();
+        configureHoundBrianResponses();
     }
 
     private void configureBindings() {
@@ -25,6 +33,24 @@ public class RobotContainer {
 
     public LEDs getLEDs() {
         return leds;
+    }
+
+    private void configureHoundBrianResponses() {
+        new Trigger(houndBrian::isZeroingActivated)
+                .onActive(() -> {
+                    leds.requestStateCommand(LEDs.LEDState.SOLID_GREEN).withTimeout(3); // Green for 3 seconds
+                    resetAllSubsystems();
+                });
+
+        new Trigger(houndBrian::isZeroingActivated)
+                .onActive(() -> leds.requestStateCommand(LEDs.LEDState.INITIALIZED_CONFIRM).schedule());
+
+        new Trigger(houndBrian::isFaultDetected)
+                .onActive(() -> leds.requestStateCommand(LEDs.LEDState.FLASHING_RED).schedule());
+    }
+
+    private void resetAllSubsystems() {
+        // reset
     }
 
 }
