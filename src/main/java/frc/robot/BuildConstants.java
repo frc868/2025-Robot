e0ddeafcd81948267a9232da08dcee977cde64df
Andy Potts <boosters.it@techhounds.com>
2025-07-09 16:21:35 -0400

    Build Constants file added

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "frc-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 194;
+  public static final String GIT_SHA = "e4fe254bcf1284a94445179c651afd93c5a75463";
+  public static final String GIT_DATE = "2025-05-26 18:21:57 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-07-09 15:54:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1752090869397L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
